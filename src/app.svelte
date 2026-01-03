a5434c9528d9a82a5ed9cb3ee1ea7d6daa664eba
PSP\Thomas.Schnoor <thomas.schnoor@edf-re.com>
2026-01-03 10:43:03 -0800

    Add SvelteKit entry points and fix vite config

--- src/app.svelte
+++ src/app.svelte
@@ -1,12 +1,5 @@
 <script>
-  import "./app.css";
+	import '../app.css';
 </script>
-
-<svelte:head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width" />
-  <title>NAIA - Industrial AI Framework</title>
-  <meta name="description" content="Transform industrial data into actionable intelligence. Coming soon." />
-</svelte:head>
 
 <slot />
