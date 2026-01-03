e9f48264b6d567a795b65fbfadee49fd594877aa
PSP\Thomas.Schnoor <thomas.schnoor@edf-re.com>
2026-01-03 10:47:28 -0800

    Add vite.config.js with SvelteKit plugin and specify Node 20 runtime

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from "@sveltejs/kit/vite";
 
-export default defineConfig({
-plugins: [svelte()],
-build: {
-target: 'esnext'
-}
-});
+export default {
+plugins: [sveltekit()]
+};
